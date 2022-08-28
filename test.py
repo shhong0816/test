d88fbaf0acb9e63e4e7ae5f4b29b1e5814336298
shhong0816 <hshhong0816@gmail.com>
2022-08-28 14:51:39 +0900

    maybe today final

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 atom 위 설정에서 package들어가서 서 toggle github tab실행.
 
 #https://www.youtube.com/watch?v=3d8waEqZZI8&ab_channel=You_Blue
@@ -21,5 +19,21 @@ git push -u origin master
 이후 오른쪽 github tab에서 코드 변경후 stage all 클릭. commit to master클릭 (commit message optional)
 
 아래에 기록들 뜨면서 github site가면 수정임.
+
+
+오른쪽 아래 github누르고 로그인도 가능
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 Changelog1
