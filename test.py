1200cd84ae73a66f2cede3db3bf89afba32bf812
shhong0816 <hshhong0816@gmail.com>
2022-08-28 14:47:59 +0900

    test1

---
+++ test.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+atom 위 설정에서 package들어가서 서 toggle github tab실행.
+
+#https://www.youtube.com/watch?v=3d8waEqZZI8&ab_channel=You_Blue
+
+폴더 제작 후 C:\Program Files\Git 의 git-cmd실행
+이후 폴더 경로로 이동. 여기 같은경우 D:\python\git\test 까지
+이후 아래꺼 입력.
+
+
+echo "# Atom" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin "https://github.com/shhong0816/test.git"
+git push -u origin master
+
+이러면 연동된거
+
+이후 오른쪽 github tab에서 코드 변경후 stage all 클릭.
